546dd9bbd41c2eecb5647eae728c508216930664
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-24 20:22:10 -0500

    added: dogseed data

---
+++ seeds/dogseeds.js
@@ -0,0 +1,17 @@
+[
+    {
+    "sex": "Male",
+    "is_stray": "true",
+    "breed": "Pug"
+},
+{
+    "sex": "Male",
+    "is_stray": "false",
+    "breed": "German Shepherd"
+},
+{
+    "sex": "Female",
+    "is_stray": "true",
+    "breed": "Pitbull"
+},
+]
