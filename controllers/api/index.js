38f6e4791f00a72443df3cbf3e4798a24ca0412b
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-24 21:30:20 -0500

    added: routes for cats and dogs in controllers folder

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+
+const catRoutes = require('./cat-routes.js');
+const dogRoutes = require('./dog-routes');
+
+router.use('/cat', catRoutes);
+router.use('/dog', dogRoutes);
+
+module.exports = router;
