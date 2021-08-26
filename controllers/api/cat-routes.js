8b6f35946e84bad06b6d0925bd96939736b58344
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-26 17:23:05 -0500

    added: GET by id route for cat

--- controllers/api/cat-routes.js
+++ controllers/api/cat-routes.js
@@ -6,8 +6,24 @@ router.get('/', async (req, res) => {
         const catData = await Cat.findAll();
         res.status(200).json(catData);
     } catch (err) {
-        res.status(400).json(err);  
+        res.status(400).json(err);
     }
 })
+
+router.get('/:id', async (req, res) => {
+    try {
+        const catData = await Cat.findByPk(req.params.id, {
+        });
+
+        if (!catData) {
+            res.status(404).json({ message: 'No cat found with this id!' });
+            return;
+        }
+
+        res.status(200).json(catData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
