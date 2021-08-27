19abc1a7b04ad854409e8f5384fac53fbd460784
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-26 20:59:18 -0500

    added: dashboard routes to index.js/ module exports

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const withAuth = require('../utils/auth');
+const router = require('./api/UserRoutes');
+
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const api = require('./api');
 
+const dashboardRoutes = require('./dashboardRoutes')
+
 
 router.use('/api', api);
+router.use('/dashboard', dashboardRoutes);
+
 
 module.exports = router;
