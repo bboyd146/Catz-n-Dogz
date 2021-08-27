edc8c485a8cbc0999b08e14ac64cff6c9ca2abc4
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-08-26 20:05:38 -0500

    userRoutes.js updated: get by id route added

--- controllers/api/UserRoutes.js
+++ controllers/api/UserRoutes.js
@@ -10,6 +10,19 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+      const userData = await User.findByPk(req.params.id,);
+      if (!userData) {
+          res.status(404).json({ message: 'No user found with that id!' });
+          return;
+      }
+      res.status(200).json(userData);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
     const userData = await User.create(req.body);
