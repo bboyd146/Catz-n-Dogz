ff4bd1e00da20901ec36cfd81e396b22519838ff
Josh Nolan <nolanjosh1979@gmail.com>
2021-08-26 22:30:10 -0500

    added GET routes for Pet

--- controllers/HomeRoutes.js
+++ controllers/HomeRoutes.js
@@ -1,7 +1,7 @@
-const route = require('express').Router();
+const router = require('express').Router();
 const User = require('../models/User');
 const withAuth = require('../utils/auth');
-const router = require('./api/UserRoutes');
+
 
 
 
--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const withAuth = require('../utils/auth');
-const router = require('./api/UserRoutes');
+
 
 module.exports = router;
