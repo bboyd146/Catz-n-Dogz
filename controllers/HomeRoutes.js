a82dac61261968df81765fa728c43be9a8429f80
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-27 12:47:15 -0500

    updated: solved relation issue with include: [{ model: Animal }] ..

--- controllers/HomeRoutes.js
+++ controllers/HomeRoutes.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const withAuth = require('../utils/auth');
-<<<<<<< HEAD
-=======
 
 
 
@@ -10,7 +8,7 @@ const withAuth = require('../utils/auth');
 
 
 
->>>>>>> ce57687b9f478c2cd07e7370ee91fd05e55b00e1
+
 
 
 module.exports = router;
