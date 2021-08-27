9dbe613cc86b87e6ba0082bf15181569458470f9
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-26 22:34:35 -0500

    refactoring routes and models

--- controllers/HomeRoutes.js
+++ controllers/HomeRoutes.js
@@ -1,13 +1,6 @@
-const route = require('express').Router();
+const router = require('express').Router();
 const User = require('../models/User');
 const withAuth = require('../utils/auth');
-const router = require('./api/UserRoutes');
-
-
-
-
-
-
 
 
 module.exports = router;
