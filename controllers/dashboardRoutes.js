489076a09744e5acf0b6a08b84a5fb7a97fc1335
Josh Nolan <nolanjosh1979@gmail.com>
2021-08-31 21:40:25 -0500

    added some dashboard routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,26 +1,35 @@
 const router = require('express').Router();
-const User = require('../models/User');
+const  { Pet, Animal } = require('../models');
 const withAuth = require('../utils/auth');
 
 
-router.get('/', withAuth, (req, res) => {
-    User.findAll({
-        where: {
-            userId: req.session.userId
-        }
-    })
-    .then(dbUserData =>{
-        const users = dbUserData.map((user) => user.get({ plain: true }));
-
-        res.render("animal-details", {
-            layout: "dashboard",
-            
+router.get('/', withAuth,  async (req, res) => {
+    try {
+        const petData = await Pet.findAll({
+            include: [
+                {
+                    model: Animal,
+                    attributes: ['animal_name'],
+                },
+            ],
         });
-    })
-    .catch(err => {
-        res.redirect("login");
-    });
+
+        const animals = petData.map((animal) =>
+            animal.get({ plain: true })
+        );
+
+        res.render('homepage', {
+            layout: 'dashboard',
+            animals,
+            loggedIn: true
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
+
+
 
 
 module.exports = router
--- public/js/login.js
+++ public/js/login.js
@@ -11,7 +11,7 @@ const loginFormHandler = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/');
+            document.location.replace('/dashboard');
         } else {
             alert('Failed to log in.');
         }
@@ -33,7 +33,7 @@ const signupFormHandler = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/');
+            document.location.replace('/dashboard');
         } else {
             alert('Failed to sign up.');
         }
