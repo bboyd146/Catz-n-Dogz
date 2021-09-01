02d05273bead75e56b8cb42244dea3d0c4161fd6
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-31 21:59:48 -0500

    work in progress

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -3,23 +3,49 @@ const User = require('../models/User');
 const withAuth = require('../utils/auth');
 
 
-router.get('/', withAuth, (req, res) => {
-    User.findAll({
-        where: {
-            userId: req.session.userId
-        }
-    })
-    .then(dbUserData =>{
-        const users = dbUserData.map((user) => user.get({ plain: true }));
+// router.get('/', withAuth, (req, res) => {
+//     User.findAll({
+//         where: {
+//             userId: req.session.userId
+//         }
+//     })
+//     .then(dbUserData =>{
+//         const users = dbUserData.map((user) => user.get({ plain: true }));
 
-        res.render("pet-details", {
-            layout: "dashboard",
-            users
+//         res.render("pet-details", {
+//             layout: "dashboard",
+//             users
+//         });
+//     })
+//     .catch(err => {
+//         res.redirect("login");
+//     });
+// });
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const petData = await Pet.findAll({
+            include: [
+                {
+                    model: Animal,
+                    attributes: ['animal_name'],
+                },
+            ],
         });
-    })
-    .catch(err => {
-        res.redirect("login");
-    });
+
+        const animals = petData.map((animal) =>
+            animal.get({ plain: true })
+        );
+
+        res.render('homepage', {
+            layout: 'main',
+            animals,
+            loggedIn: true,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 
