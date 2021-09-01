b9663448bcbf565ddf6689e17900780388828785
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-31 20:20:32 -0500

    created seeds for Post model

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,11 @@
 const sequelize = require('../config/connection');
-const { Animal, Pet } = require('../models');
+const { Animal, Pet, Post } = require('../models');
 const User = require('../models/User');
 
 const animalSeedData = require('./animalseeds');
 const petSeedData = require('./petseeds');
 const userData = require('./userData.json');
+const postData = require('./postseeds.json');
 
 console.log(animalSeedData)
 
@@ -12,6 +13,8 @@ const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
     const animals = await Animal.bulkCreate(animalSeedData);
+
+    const posts = await Post.bulkCreate(postData);
 
 
     const pets = await Pet.bulkCreate(petSeedData);
